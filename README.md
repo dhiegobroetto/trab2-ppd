8be061573f9304cda30cacc145fb8cb14fb3343d
Dhiego Broetto <dhiegobroetto@hotmail.com>
2019-06-20 14:24:16 -0300

    Rollback

---
+++ README.md
@@ -0,0 +1,46 @@
+# Trabalho 2 de PPD
+Ataque de Dicionário em Mensagem Criptografada com Middleware Orientado a Mensagens.
+
+## Pré requisitos:
+### Glassfish 5
+Como instalar:
+No linux:
+
+`wget http://download.oracle.com/glassfish/5.0/release/glassfish-5.0.zip`
+
+E descompacte o zip em um diretório qualquer.
+
+Entrar na pasta bin do glassfish e executar:
+
+`./asadmin start-domain`
+
+Para iniciar o servidor do glassfish (isso pode levar alguns minutos).
+
+É necessário importar o arquivo XML de configuração para as filas. Para isso basta executar, ainda na pasta bin do glassfish:
+
+`./asadmin add-resources <diretório do arquivo>/<nome do arquivo>.xml`
+
+# Compilar e executar
+
+Com as filas importadas, agora podemos compilar o projeto.
+
+Execute na pasta raíz do projeto o comando:
+
+`java -d bin -cp .:/<diretório do glassfish>/lib/gf-client.jar src/*.java`
+
+Para executar cada arquivo, basta executar:
+
+`java -cp .:./bin:/<diretório do glassfish>/lib/gf-client.jar <nome da classe>`
+
+# Especificações:
+
+O objetivo deste trabalho é praticar programação paralela usando middleware orientado a mensagens JMS e realizar análise de desempenho em um cluster de computadores. O trabalho inclui a comparação desta implementação com a implementação do primeiro trabalho (baseada em Request-Response com Java RMI).
+
+A ideia agora é calcular o **m**, que será uma variável que iremos utilizar para definir a quantidade de mensagens que irão ser enviados aos demais escravos, anteriormente utilizávamos a quantidade de escravos ativos no sistema.
+
+Adotamos MapMessage como a estrutura de mensagens que será enviada às filas, com as informações:
+```java
+byte[] ciphertext, byte[] knowntext, long initialwordindex, long finalwordindex, int attackNumber;
+```
+
+O cabeçalho das mensagens dos escravos para o master, através da fila _GuessesQueue_, haverá o _attackNumber_. Isso será feito para o mestre saber onde direcionar o guess encontrado.
---
+++ src/br/inf/ufes/ppd/client/ClientExecute.java
@@ -0,0 +1,85 @@
+package br.inf.ufes.ppd.client;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Random;
+
+import br.inf.ufes.ppd.Decrypt;
+import br.inf.ufes.ppd.Guess;
+import br.inf.ufes.ppd.master.Master;
+
+public class ClientExecute {
+	public static void main(String[] args) {
+		try {
+			Guess[] guesses = null;
+			byte[] ciphertext = null;
+			if (args.length < 3) {
+				int fileNumber = 1; // Número do arquivo, alterar quando iniciar novos escravos.
+				PrintWriter writer = new PrintWriter("result" + fileNumber + "Slave.csv", "UTF-8");
+				for (int i = 1; i <= 10; i++) {
+					byte[] knowntext = args[1].getBytes();
+					Registry registry = LocateRegistry.getRegistry(args[0]);
+					Master master = (Master) registry.lookup("mestre");
+					ciphertext = Decrypt.readFile("../files/" + (i * 10000) + ".cipher");
+					long initialTime = System.nanoTime();
+					guesses = master.attack(ciphertext, knowntext);
+					long finalTime = System.nanoTime();
+					// Date totalDate = new Date(finalTime - initialTime);
+					long totalTime = finalTime - initialTime;
+					double secondsTime = (totalTime / 1_000_000_000.0);
+					System.out.println("File no." + i + "; Total time: " + secondsTime + " seconds.");
+					writer.println("Time;" + secondsTime + ";Nano;" + totalTime + ";");
+				}
+				writer.close();
+				for (Guess guess : guesses) {
+					if (guess == null)
+						break;
+					Decrypt.saveFile(guess.getKey() + ".msg", guess.getMessage());
+				}
+				System.exit(0);
+			} else {
+				try {
+					ciphertext = Decrypt.readFile(args[2]);
+				} catch (FileNotFoundException e) {
+					Random rd = new Random();
+					int fileSize;
+					if (args.length < 4)
+						fileSize = rd.nextInt(99000) + 1000;
+					else
+						fileSize = Integer.parseInt(args[3]);
+					fileSize -= fileSize % 8;
+					byte[] file = new byte[fileSize];
+					rd.nextBytes(file);
+					ciphertext = file;
+				}
+			}
+			Registry registry = LocateRegistry.getRegistry(args[0]);
+			Master master = (Master) registry.lookup("mestre");
+			byte[] knowntext = args[1].getBytes();
+			long initialTime = System.nanoTime();
+			guesses = master.attack(ciphertext, knowntext);
+			long finalTime = System.nanoTime();
+			long totalTime = finalTime - initialTime;
+			double secondsTime = (double) (totalTime / 1_000_000_000.0);
+			System.out.println("Total time: " + secondsTime + " seconds.");
+			for (Guess guess : guesses) {
+				if (guess == null)
+					break;
+				Decrypt.saveFile(guess.getKey() + ".msg", guess.getMessage());
+			}
+		} catch (RemoteException e) {
+			System.out.println("[Error] RemoteException in ClientExecute.");
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			System.out.println("[Error] NotBoundException in ClientExecute.");
+		} catch (IOException e) {
+			System.out.println("[Error] IOException in ClientExecute.");
+			e.printStackTrace();
+		}
+	}
+}
